f52cdf3384a3c47ac0a97cde605ec196fba0fd4b
bhyerain <seunghye0307@naver.com>
2025-11-21 22:48:06 +0900

    [feat] TanStack Query 세팅

--- src/apps/main.tsx
+++ src/apps/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/apps/App.tsx";
+import { QueryProvider } from "@/shared/query/query-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </StrictMode>
 );
