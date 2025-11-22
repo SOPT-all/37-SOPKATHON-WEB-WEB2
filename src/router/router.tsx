7b4d9a1eaaa9ab8ae782e2e7064c4aa2f63b2ad5
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-22 23:47:26 +0900

    feat: 로딩 컴포넌트 추가

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,10 +1,12 @@
 import { lazy, Suspense } from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 
-const HomePage = lazy(() => import('@/pages/HomePage'));
+import Loading from '@/shared/components/loading/Loading';
 
 import { ROUTES } from './constant/routes';
 import Layout from './layout';
+
+const HomePage = lazy(() => import('@/pages/HomePage'));
 
 export const router = createBrowserRouter([
   {
@@ -13,7 +15,7 @@ export const router = createBrowserRouter([
       {
         path: ROUTES.HOME,
         element: (
-          <Suspense fallback={<div>Loading...</div>}>
+          <Suspense fallback={<Loading />}>
             <HomePage />
           </Suspense>
         ),
