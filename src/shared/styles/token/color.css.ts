c3b1e818c6a6fb2ff28471b2899a1107677da9ed
bhyerain <seunghye0307@naver.com>
2025-11-23 00:34:26 +0900

    feat: 컬러코드 추가 및 분류

--- src/shared/styles/token/color.css.ts
+++ src/shared/styles/token/color.css.ts
@@ -1,12 +1,45 @@
 export const color = {
-  gray50: '#FAFAFA',
-  gray100: '#F5F5F5',
-  gray200: '#EEEEEE',
-  gray300: '#E0E0E0',
-  gray400: '#BDBDBD',
-  gray500: '#9E9E9E',
-  gray600: '#757575',
-  gray700: '#616161',
-  gray800: '#424242',
-  gray900: '#212121',
+  gray: {
+    g50: '#FAFAFA',
+    g100: '#F5F5F5',
+    g200: '#EEEEEE',
+    g300: '#E0E0E0',
+    g400: '#BDBDBD',
+    g500: '#9E9E9E',
+    g600: '#757575',
+    g700: '#616161',
+    g800: '#424242',
+    g900: '#212121',
+  },
+
+  primary: {
+    black: '#121212',
+    p900: '#276869',
+    p700: '#42AFB1',
+    p500: '#5DF7F9',
+    p300: '#92FAFB',
+    p100: '#CDFDFD',
+    p50: '#EFFEFE',
+  },
+
+  secondary: {
+    s900: '#34206B',
+    s700: '#5837B5',
+    s500: '#7C4DFF',
+    s300: '#A788FF',
+    s100: '#D6C8FF',
+    s50: '#F2EDFF',
+  },
+
+  common: {
+    black: '#000000',
+    white: '#FFFFFF',
+  },
+
+  error: {
+    e4: '#B70000',
+    e3: '#FF4444',
+    e2: '#FFA6A6',
+    e1: '#C9C9C9',
+  },
 };
