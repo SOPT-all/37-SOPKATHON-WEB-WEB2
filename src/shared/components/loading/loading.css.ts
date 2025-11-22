d66b769371f8b30b5f3b078dda2020fda9d578a2
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-23 08:18:01 +0900

    add: 파비콘 추가

--- src/shared/components/loading/loading.css.ts
+++ src/shared/components/loading/loading.css.ts
@@ -1,5 +1,34 @@
 import { style } from '@vanilla-extract/css';
 
+import { themeVars } from '@/shared/styles/theme.css';
+
+export const pageContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  minHeight: 'calc(100dvh - 5.6rem)', // AppBar 높이 제외
+});
+
 export const loadingStyle = style({
   display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '1.6rem',
+});
+
+export const textContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  ...themeVars.fontStyles.display1_sb_32,
+});
+
+export const pointText = style({
+  backgroundColor: themeVars.color.black,
+  color: themeVars.color.primary500,
+  borderRadius: '4px',
+  padding: '0 0.8rem',
 });
